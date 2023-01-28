144fd211b64a11e3ed69058ba89d65b946150ebe
Yuri-Butorin-SPb <118922113+Yuri-Butorin-SPb@users.noreply.github.com>
2023-01-28 11:09:36 +0300

    add Ex_1

---
+++ Ex_1/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] arr = new string [4];
+int count = arr.Length;
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Введите {i+1} слово: ");
+    arr[i] = Console.ReadLine();
+}
+
+int sum = 0; // Считаю, сколько элементов массива имеют 3 и менее символов
+
+for (int i = 0; i < count; i++)
+{
+    if (arr[i].Length <= 3) sum++;
+}
+
+
+string[] arr2 = new string [sum]; // Добавляю элементы с первого массива во второй
+
+for (int i = 0; i < count; i++) 
+{
+    if (arr[i].Length <= 3) arr2[i] = arr[i];
+}
+
+
+PrintArray(arr2);
+
+
+void PrintArray(string[] array) // Печатаю второй массив
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
